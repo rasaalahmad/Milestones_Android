cab870a515eb1786c52dbc95e51173c4a0e6ca10
Rasaal Ahmad <56187211+rasaalahmad@users.noreply.github.com>
2023-01-19 14:48:41 +0500

    API Unit testing Completed

--- app/src/main/java/com/example/milestone2/data_classes/Meme.kt
+++ app/src/main/java/com/example/milestone2/data_classes/Meme.kt
@@ -1,3 +1,5 @@
 package com.example.milestone2.data_classes
 
-data class Meme(var id:String?, var name:String?, var url:String?, var width:Int?, var height:Int?, var boxCount:Int?)
+data class Meme(var id:String?, var name:String?, var url:String?, var width:Int?, var height:Int?, var boxCount:Int?) {
+    constructor() : this(null,null,null,0,0,0)
+}
